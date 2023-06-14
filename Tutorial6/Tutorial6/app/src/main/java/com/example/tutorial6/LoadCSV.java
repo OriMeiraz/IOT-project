fcc4df72cc678a5d664d53279777ebb2f5c2533a
OriMeiraz <ori.meiraz@campus.technion.ac.il>
2023-06-14 16:11:26 +0300

    app has sound and distance

---
+++ Tutorial6/Tutorial6/app/src/main/java/com/example/tutorial6/LoadCSV.java
@@ -0,0 +1,114 @@
+package com.example.tutorial6;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.opencsv.CSVReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import java.util.List;
+
+
+public class LoadCSV extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_csv);
+        Button BackButton = (Button) findViewById(R.id.button_back);
+        Button display = (Button) findViewById(R.id.load_csv_btn);
+        EditText name = findViewById(R.id.load_csv_file);
+        LineChart lineChart = (LineChart) findViewById(R.id.line_chart);
+        /*
+
+
+         */
+
+
+        BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClickBack();
+            }
+        });
+
+        display.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String[]> csvData = new ArrayList<>();
+
+                csvData= CsvRead("/sdcard/csv_dir/"+name.getText());
+                csvData= CsvRead("/sdcard/csv_dir/"+name.getText());
+                LineDataSet xDataSet =  new LineDataSet(DataValues(csvData, 1),"x");
+                xDataSet.setColor(ColorTemplate.rgb("ff0000"));
+                xDataSet.setCircleColor((ColorTemplate.rgb("ff0000")));
+
+                LineDataSet yDataSet =  new LineDataSet(DataValues(csvData, 2),"y");
+                yDataSet.setColor(ColorTemplate.rgb("00ff00"));
+                yDataSet.setCircleColor(ColorTemplate.rgb("00ff00"));
+
+                LineDataSet zDataSet =  new LineDataSet(DataValues(csvData, 3),"z");
+                zDataSet.setColor(ColorTemplate.rgb("0000ff"));
+                zDataSet.setCircleColor(ColorTemplate.rgb("0000ff"));
+
+                ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+
+                dataSets.add(xDataSet);
+                dataSets.add(yDataSet);
+                dataSets.add(zDataSet);
+
+                LineData data = new LineData(dataSets);
+                lineChart.setData(data);
+                lineChart.invalidate();
+            }
+        });
+    }
+
+    private void ClickBack(){
+        finish();
+
+    }
+
+    private ArrayList<String[]> CsvRead(String path){
+        ArrayList<String[]> CsvData = new ArrayList<>();
+        try {
+            File file = new File(path);
+            CSVReader reader = new CSVReader(new FileReader(file));
+            String[] nextline;
+            while((nextline = reader.readNext())!= null){
+                if(nextline != null){
+                    CsvData.add(nextline);
+                }
+            }
+
+        }catch (Exception e){}
+        return CsvData;
+    }
+
+    private ArrayList<Entry> DataValues(ArrayList<String[]> csvData, int index){
+        ArrayList<Entry> dataVals = new ArrayList<Entry>();
+        for (int i = 6; i < csvData.size(); i++){
+            dataVals.add(new Entry(Float.parseFloat(csvData.get(i)[0]),
+                    Float.parseFloat(csvData.get(i)[index])));
+
+
+        }
+
+        return dataVals;
+    }
+
+}
--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,16 +1,25 @@
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
 const int trigPin = 5;
 const int echoPin = 18;
 
 //define sound speed in cm/uS
 #define SOUND_SPEED 0.034
 #define CM_TO_INCH 0.393701
-
+const float alpha = 0.9;
 long duration;
 float distanceCm;
 float distanceInch;
 
 void setup() {
   Serial.begin(9600); // Starts the serial communication
+  SerialBT.begin("DolevThePrince"); //Bluetooth device name
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 }
@@ -28,13 +37,13 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
   
   // Calculate the distance
-  distanceCm = duration * SOUND_SPEED/2;
-  
-  // Convert to inches
+  distanceCm = alpha * distanceCm + (1-alpha) * duration * SOUND_SPEED/2;
   
   // Prints the distance in the Serial Monitor
-  Serial.print("Distance (cm): ");
-  Serial.println(distanceCm);
+  //Serial.print("Distance (cm): ");
+  Serial.println((String)+"Distance:"+distanceCm);
+  SerialBT.println((String)+"Distance:"+distanceCm); 
+
   
-  delay(100);
+  delay(50);
 }
